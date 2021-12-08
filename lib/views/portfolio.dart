394d7d6517a8063a90337ba665a70ca2858e2182
Karan <karan@Applications-MacBook-Pro.local>
2021-12-08 00:09:19 -0500

    code cleanup

--- lib/views/portfolio.dart
+++ lib/views/portfolio.dart
@@ -84,40 +84,4 @@ class _Portfolio extends State<Portfolio> {
           ),
         ));
   }
-
-  // void showLogoutDialog(BuildContext context) {
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         title: const Text("Logout"),
-  //         content: SingleChildScrollView(
-  //           child: ListBody(
-  //             children: const <Widget>[
-  //               Text('Are you sure you want to log out?'),
-  //             ],
-  //           ),
-  //         ),
-  //         actions: <Widget>[
-  //           TextButton(
-  //             child: const Text("NO"),
-  //             onPressed: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //           ),
-  //           TextButton(
-  //             child: const Text("YES"),
-  //             onPressed: () async {
-  //               await auth.signOut();
-  //               Navigator.push(
-  //                 context,
-  //                 MaterialPageRoute(builder: (context) => const PaperStox()),
-  //               );
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-  // }
 }
